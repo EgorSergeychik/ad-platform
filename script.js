54914d6849930e9790442d544a64468145b842d3
Egor Sergeychik <sergeychike.egor@gmail.com>
2024-05-04 13:47:28 +0300

    refactor: remove useless code

--- script.js
+++ script.js
@@ -18,29 +18,3 @@ function printDeveloperInfo(surname, name, position = "Розробник") {
 
 printDeveloperInfo("Сєргєйчик", "Єгор");
 
-function search() {
-    let search = document.querySelector('#search input')
-    let search_value = search.value
-
-    if (search_value > 'Пошук') {
-        alert(search)
-    } else {
-        alert('Пошук')
-    }
-}
-
-function changeColor(time = 3000, color = '#df7575') {
-    let defaultColor = document.body.style.backgroundColor
-
-    document.body.style.backgroundColor = color
-
-    setTimeout(() => document.body.style.backgroundColor = defaultColor, time)
-
-}
-
-function newCard() {
-    let card = document.querySelectorAll('.card')
-    card = card[card.length - 1]
-
-    card.innerHTML = '<a href="ad.html"><img src="https://picsum.photos/200/266"><div class="card-content"><p class="card-title">innerHTML</p></p></div></div></a>'
-}
