7cbd0645145b0c5c5a68f6453ae2342cea7a7e17
Egor Sergeychik <sergeychike.egor@gmail.com>
2024-05-18 17:43:10 +0300

    refactor: remove unused code

--- temp.js
+++
@@ -1,7 +0,0 @@
-form.addEventListener('dblclick', {
-    handleEvent(event) {
-        console.log("currentTarget", event.currentTarget);
-        console.log("target", event.target);
-        console.log("-------------------");
-    }
-});
